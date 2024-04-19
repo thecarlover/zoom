8541e971196c65e2687e2fd0b321eee5f41ff853
thecarlover <s1mittal@hotmail.com>
2024-04-20 00:08:48 +0530

    page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,13 +13,13 @@ const Home = () => {
   
   
   return (
-    <section className='flex size-full flex-col gap-10 text-dark'>
+    <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[400px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
           <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
             Upcoming Meeting at:12:30 PM
           </h2>
-          //time 
+          
           <div className='flex flex-col gap-2'>
             <h1 className='texst-4xl font-extrabold lg:text-7xl'>
               {time}
