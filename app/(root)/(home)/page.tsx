38f8103a086c8739a529ee8b15bff18345f24418
thecarlover <s1mittal@hotmail.com>
2024-04-20 00:06:49 +0530

    home page update

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
 const Home = () => {
+
+  const now=new Date();
+
+  const time=now.toLocaleString('en-US',{hour:'2-digit',minute:'2-digit'});
+
+  const date=(new Intl.DateTimeFormat('en-US',{
+    dateStyle:'full'
+  })).format(now);
+  
+  
+  
   return (
-    <section className='flex size-full flex-col gap-10 text-[#8e50f1]'>
-      <h1 className='text-3xl font-bold'>
-        Home
-      </h1>
+    <section className='flex size-full flex-col gap-10 text-dark'>
+      <div className='h-[400px] w-full rounded-[20px] bg-hero bg-cover'>
+        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
+            Upcoming Meeting at:12:30 PM
+          </h2>
+          //time 
+          <div className='flex flex-col gap-2'>
+            <h1 className='texst-4xl font-extrabold lg:text-7xl'>
+              {time}
+            </h1>
+            <p className='text-lg font-medium text-sky-200 lg:text-2xl'>{date}</p>
+
+          </div>
+        </div>
+
+      </div>
 
     </section>
   )
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -23,7 +23,7 @@ const MobileNav = () => {
             className="cursor-pointer sm:hidden"
           />
         </SheetTrigger>
-        <SheetContent side="bottom" className="border-none bg-dark-1">
+        <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
             <Image
               src="/icons/logo.svg"
